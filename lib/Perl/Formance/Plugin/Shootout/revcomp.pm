6f9ed60d24a19bc4e0f3b89e6dfd69297828c8d9
Steffen Schwigon <ss5@renormalist.net>
2010-02-24 23:44:58 +0100

    rough benchmark code cut'n'pasted from web page

---
+++ lib/Perl/Formance/Plugin/Shootout/revcomp.pm
@@ -0,0 +1,34 @@
+package Perl::Formance::Plugin::Shootout::revcomp;
+
+COMMAND LINE:
+/usr/bin/perl revcomp.perl-4.perl 0 < revcomp-input25000000.txt
+
+# The Computer Language Benchmarks Game
+# http://shootout.alioth.debian.org/
+#
+# Contributed by Andrew Rodland
+
+use strict;
+
+sub print_reverse {
+  while (my $chunk = substr $_[0], -60, 60, '') {
+    print scalar reverse($chunk), "\n";
+  }
+}
+
+my $data;
+
+while (<STDIN>) {
+  if (/^>/) {
+    print_reverse $data;
+    print;
+  } else {
+    chomp;
+    tr{wsatugcyrkmbdhvnATUGCYRKMBDHVN}
+      {WSTAACGRYMKVHDBNTAACGRYMKVHDBN};
+    $data .= $_;
+  }
+}
+print_reverse $data;
+
+1;
