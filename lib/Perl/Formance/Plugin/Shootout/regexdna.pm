74131168711cb90677ac08b3389f99967751915b
Steffen Schwigon <ss5@renormalist.net>
2010-03-01 08:44:38 +0100

    regexdna as Perl::Formance plugin

--- lib/Perl/Formance/Plugin/Shootout/regexdna.pm
+++ lib/Perl/Formance/Plugin/Shootout/regexdna.pm
@@ -1,7 +1,7 @@
 package Perl::Formance::Plugin::Shootout::regexdna;
 
-COMMAND LINE:
-/usr/bin/perl regexdna.perl-2.perl 0 < regexdna-input5000000.txt
+# COMMAND LINE:
+# /usr/bin/perl regexdna.perl-2.perl 0 < regexdna-input5000000.txt
 
 # The Computer Language Benchmarks Game
 # http://shootout.alioth.debian.org/
@@ -10,59 +10,92 @@ COMMAND LINE:
 # cleaned up for speed and fun by Mirco Wahab
 # improved STDIN read, regex clean up by Jake Berner
 # More speed and multithreading by Andrew Rodland
+# Perl::Formance plugin by Steffen Schwigon
 
 use strict;
 use warnings;
 
-my $l_file  = -s STDIN;
-my $content; read STDIN, $content, $l_file;
-# this is significantly faster than using <> in this case
+use File::ShareDir qw(module_dir);
+use Benchmark ':hireswallclock';
 
-$content =~ s/^>.*//mg;
-$content =~ tr/\n//d;
-my $l_code  =  length $content;
+sub run
+{
+        my ($infile) = @_;
 
-my @seq = ( 'agggtaaa|tttaccct',
-        '[cgt]gggtaaa|tttaccc[acg]',
-        'a[act]ggtaaa|tttacc[agt]t',
-        'ag[act]gtaaa|tttac[agt]ct',
-        'agg[act]taaa|ttta[agt]cct',
-        'aggg[acg]aaa|ttt[cgt]ccct',
-        'agggt[cgt]aa|tt[acg]accct',
-        'agggta[cgt]a|t[acg]taccct',
-        'agggtaa[cgt]|[acg]ttaccct' );
+        my $srcdir  = module_dir(__PACKAGE__);
+        my $srcfile = "$srcdir/$infile";
+        open FILE, "<", $srcfile or die "Cannot read $srcfile";
+        my $l_file  = -s FILE;
+        my $content; read FILE, $content, $l_file;
+        # this is significantly faster than using <> in this case
 
-my @procs;
-for my $s (@seq) {
-  my $pat = qr/$s/;
-  my $pid = open my $fh, '-|';
-  defined $pid or die "Error creating process";
-  unless ($pid) {
-    my $cnt = 0;
-    ++$cnt while $content =~ /$pat/gi;
-    print "$s $cnt\n";
-    exit 0;
-  }
-  push @procs, $fh;
+        $content =~ s/^>.*//mg;
+        $content =~ tr/\n//d;
+        my $l_code  =  length $content;
+
+        my @seq = ( 'agggtaaa|tttaccct',
+                    '[cgt]gggtaaa|tttaccc[acg]',
+                    'a[act]ggtaaa|tttacc[agt]t',
+                    'ag[act]gtaaa|tttac[agt]ct',
+                    'agg[act]taaa|ttta[agt]cct',
+                    'aggg[acg]aaa|ttt[cgt]ccct',
+                    'agggt[cgt]aa|tt[acg]accct',
+                    'agggta[cgt]a|t[acg]taccct',
+                    'agggtaa[cgt]|[acg]ttaccct' );
+
+        my @procs;
+        for my $s (@seq) {
+                my $pat = qr/$s/;
+                my $pid = open my $fh, '-|';
+                defined $pid or die "Error creating process";
+                unless ($pid) {
+                        my $cnt = 0;
+                        ++$cnt while $content =~ /$pat/gi;
+                        #print "$s $cnt\n";
+                        exit 0;
+                }
+                push @procs, $fh;
+        }
+
+        for my $proc (@procs) {
+                print <$proc>;
+                close $proc;
+        }
+
+        my %iub = (         B => '(c|g|t)',  D => '(a|g|t)',
+                            H => '(a|c|t)',   K => '(g|t)',    M => '(a|c)',
+                            N => '(a|c|g|t)', R => '(a|g)',    S => '(c|g)',
+                            V => '(a|c|g)',   W => '(a|t)',    Y => '(c|t)' );
+
+        # We could cheat here by using $& in the subst and doing it inside a string
+        # eval to "hide" the fact that we're using $& from the rest of the code... but
+        # it's only worth 0.4 seconds on my machine.
+        my $findiub = '(['.(join '', keys %iub).'])';
+
+        $content =~ s/$findiub/$iub{$1}/g;
+
+        return {
+                length_file    => $l_file,
+                length_code    => $l_code,
+                length_content => length $content,
+               };
 }
 
-for my $proc (@procs) {
-  print <$proc>;
-  close $proc;
+sub main
+{
+        my ($options) = @_;
+
+        my $goal   = $ENV{PERLFORMANCE_TESTMODE_FAST} ? "regexdna-input-mini.txt" : "regexdna-input.txt";
+        my $count  = $ENV{PERLFORMANCE_TESTMODE_FAST} ? 1 : 5;
+
+        my $result;
+        my $t = timeit $count, sub { $result = run($goal) };
+        return {
+                Benchmark     => $t,
+                goal          => $goal,
+                count         => $count,
+                result        => $result,
+               };
 }
-
-my %iub = (         B => '(c|g|t)',  D => '(a|g|t)',
-  H => '(a|c|t)',   K => '(g|t)',    M => '(a|c)',
-  N => '(a|c|g|t)', R => '(a|g)',    S => '(c|g)',
-  V => '(a|c|g)',   W => '(a|t)',    Y => '(c|t)' );
-
-# We could cheat here by using $& in the subst and doing it inside a string
-# eval to "hide" the fact that we're using $& from the rest of the code... but
-# it's only worth 0.4 seconds on my machine.
-my $findiub = '(['.(join '', keys %iub).'])';
-
-$content =~ s/$findiub/$iub{$1}/g;
-
-printf "\n%d\n%d\n%d\n", $l_file, $l_code, length $content;
 
 1;
