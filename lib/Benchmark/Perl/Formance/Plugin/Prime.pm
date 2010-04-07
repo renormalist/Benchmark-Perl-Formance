78560fc0344e89693737958c9260f451545d27bb
Steffen Schwigon <ss5@renormalist.net>
2010-04-07 14:49:22 +0200

    prime numbers
    I do not know yet whether that's useful. Finding out what's
    useful is part of the mission so I just added, huh, rocket
    science: prime numbers.

---
+++ lib/Benchmark/Perl/Formance/Plugin/Prime.pm
@@ -0,0 +1,74 @@
+package Benchmark::Perl::Formance::Plugin::Prime;
+
+# Prime numbers
+
+use strict;
+use warnings;
+
+use vars qw($goal $count);
+$goal  = $ENV{PERLFORMANCE_TESTMODE_FAST} ? 15 : 22;
+$count = 5;
+
+use Benchmark ':hireswallclock';
+
+sub math_primality
+{
+        my ($options) = @_;
+
+        eval "use Math::Primality 'next_prime'";
+        if ($@) {
+                print STDERR "# ".$@ if $options->{verbose} > 2;
+                return { failed => "use Math::Primality failed" };
+        }
+
+        my $t;
+        my $result;
+        my $numgoal = 10 ** $goal;
+        $t = timeit $count, sub { $result = next_prime($numgoal) };
+        return {
+                Benchmark => $t,
+                result    => "$result", # stringify from blessed
+                goal      => $goal,
+                numgoal   => $numgoal,
+               };
+}
+
+sub crypt_primes
+{
+        my ($options) = @_;
+
+        eval "use Crypt::Primes 'maurer'";
+        if ($@) {
+                print STDERR "# ".$@ if $options->{verbose} > 2;
+                return { failed => "use Crypt::Primes failed" };
+        }
+
+        my $t;
+        my $result;
+        my $bitgoal = int($goal * 3.36); # bitness in about same size order as int length in math_primality
+        print STDERR "bitgoal: $bitgoal\n";
+        $t = timeit $count, sub { $result = maurer(Size => $bitgoal) };
+        return {
+                Benchmark => $t,
+                result    => "$result", # stringify from blessed
+                goal      => $goal,
+                bitgoal   => $bitgoal,
+               };
+}
+
+sub main {
+        return {
+                math_primality => math_primality(),
+                crypt_primes   => crypt_primes(),
+               };
+}
+1;
+
+__END__
+
+=head1 NAME
+
+Benchmark::Perl::Formance::Plugin::Prime - Stress math libs (Math::GMPz)
+
+=cut
+
