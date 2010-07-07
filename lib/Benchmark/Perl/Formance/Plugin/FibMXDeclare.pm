5e48cfd59c96ce35f8d6a84db5b4ca5deee70270
Steffen Schwigon <ss5@renormalist.net>
2010-07-07 23:23:47 +0200

    new benchmark to stress MooseX::Declare method calls

---
+++ lib/Benchmark/Perl/Formance/Plugin/FibMXDeclare.pm
@@ -0,0 +1,50 @@
+package Benchmark::Perl::Formance::Plugin::FibMXDeclare;
+
+# Fibonacci numbers
+
+use strict;
+use warnings;
+
+our $goal;
+our $count;
+
+use Benchmark ':hireswallclock';
+
+use MooseX::Declare;
+
+class Fib {
+        method fib($n) {
+                $n < 2
+                  ? 1
+                    : $self->fib($n-1) + $self->fib($n-2);
+        }
+}
+
+sub main
+{
+        my ($options) = @_;
+
+        # ensure same values over all Fib* plugins!
+        $goal  = $options->{fastmode} ? 18 : 35;
+        $count = 5;
+
+        my $result;
+        my $fib = Fib->new;
+        my $t   = timeit $count, sub { $result = $fib->fib($goal) };
+        return {
+                Benchmark => $t,
+                result    => $result,
+                goal      => $goal,
+               };
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Benchmark::Perl::Formance::Plugin::FibMXDeclare - Stress recursion and method calls (MooseX::Declare)
+
+=cut
+
