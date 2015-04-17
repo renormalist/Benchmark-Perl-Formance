37ce0eebdd5a62b75d7800d262d91661bc256e13
Steffen Schwigon <ss5@renormalist.net>
2015-04-17 22:50:28 +0200

    experiments

--- evaluation/3d.r
+++ evaluation/3d.r
@@ -4,7 +4,7 @@ d <- read.table(file='example-data.txt', header=TRUE)
 
 library(latticeExtra)
 
-cloud(y~x+z, d, panel.3d.cloud=panel.3dbars, col.facet='grey', 
+cloud(value~version+cfg_t_64, d, panel.3d.cloud=panel.3dbars, col.facet='steelblue',
       xbase=0.4, ybase=0.4, scales=list(arrows=FALSE, col=1),
       par.settings = list(axis.line = list(col = "transparent")))
       
