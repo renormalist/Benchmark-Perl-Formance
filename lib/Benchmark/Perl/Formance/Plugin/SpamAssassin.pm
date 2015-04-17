01f90e057df51474c0f56192fac77b97bcc23ec8
Steffen Schwigon <ss5@renormalist.net>
2015-04-17 15:34:57 +0200

    rework plugin:SpamAssassin
    - Run sa-learn in default behaviour.
    - Run ham and spam with different available
      data sets.
    - Code cleanup.

--- lib/Benchmark/Perl/Formance/Plugin/SpamAssassin.pm
+++ lib/Benchmark/Perl/Formance/Plugin/SpamAssassin.pm
@@ -24,62 +24,60 @@ use Benchmark ':hireswallclock';
 sub main {
         my ($options) = @_;
 
-        $count    = $options->{fastmode} ? 1 : 5;
-        $easy_ham = $options->{fastmode} ? "easy_ham_subset" : "easy_ham";
-
-        my $dstdir = tempdir( CLEANUP => 1 );
         my $srcdir; eval { $srcdir = dist_dir('Benchmark-Perl-Formance-Cargo')."/SpamAssassin" };
-        return { salearn => { failed => "no Benchmark-Perl-Formance-Cargo" } } if $@;
+        if ($@) {
+                return { salearn => { failed => "no Benchmark-Perl-Formance-Cargo" } }
+        }
 
-        print STDERR "# Prepare cargo spam'n'ham files in $dstdir ...\n" if $options->{verbose} >= 3;
-
-        dircopy($srcdir, $dstdir);
-
-        (my $salearn     = $^X) =~ s!/perl[\d.]*$!/sa-learn!;
-        my $salearncfg   = "$dstdir/sa-learn.cfg";
-        my $salearnprefs = "$dstdir/sa-learn.prefs";
-
-        print STDERR "# Use sa-learn: $salearn\n"      if $options->{verbose} >= 3;
-        print STDERR "#          cfg: $salearncfg\n"   if $options->{verbose} >= 3;
-        print STDERR "#        prefs: $salearnprefs\n" if $options->{verbose} >= 3;
-
+        (my $salearn = $^X) =~ s!/perl[\d.]*$!/sa-learn!;
         if (not $salearn && -x $salearn) {
                 print STDERR "# did not find executable $salearn\n" if $options->{verbose} >= 2;
-                return {
-                        salearn => {
-                                    failed       => "did not find executable sa-learn",
-                                    salearn_path => $salearn,
-                                   }
-                       };
+                return { salearn => { failed => "did not find executable sa-learn", salearn_path => $salearn } };
         }
 
-        # spam variant:
-        # my $cmd    = "time /usr/bin/env perl -T $salearn --spam -L --config-file=$salearncfg --prefs-file=$salearnprefs --siteconfigpath=$dstdir --dbpath=$dstdir/db --no-sync  '$dstdir/spam_2/*'";
-        # ham variant:
-        chmod 0666, $salearncfg;
-        if (open SALEARNCFG, ">", $salearncfg) {
-                print SALEARNCFG "loadplugin Mail::SpamAssassin::Plugin::Bayes\n";
-                close SALEARNCFG;
-        } else {
-                print STDERR "# could not write sa-learn.cfg: $salearncfg" if $options->{verbose} >= 2;
+        $count     = $options->{fastmode} ? 1 : 5;
+        my @passes = $options->{fastmode} ? (
+                                             { metric => "ham",
+                                               type   => "ham",
+                                               subdir => "easy_ham_subset",
+                                             },
+                                            ) :
+                                             (
+                                              { metric => "ham",
+                                                type   => "ham",
+                                                subdir => "easy_ham",
+                                              },
+                                              { metric => "ham2",
+                                                type   => "ham",
+                                                subdir => "easy_ham_2",
+                                              },
+                                              { metric => "spam",
+                                                type   => "spam",
+                                                subdir => "spam",
+                                              },
+                                              { metric => "spam2",
+                                                type   => "spam",
+                                                subdir => "spam_2",
+                                              },
+                                             );
+
+        # sa-learn
+        my %results;
+        for my $pass (@passes) {
+                my @output;
+                my $cmd       = "$^X -T $salearn --$pass->{type} -L --no-sync '$srcdir/$pass->{subdir}' 2>&1";
+                print STDERR "\n# $cmd\n" if $options->{verbose} >= 4;
+                my $t         = timeit $count, sub { @output = map { chomp; $_ } qx($cmd) };
+                my $maxerr    = ($#output < 10) ? $#output : 10;
+                print STDERR join("\n# ", "", @output[0..$maxerr]) if $options->{verbose} >= 4;
+
+                $results{salearn}{$pass->{metric}} = {
+                                                      Benchmark    => [@$t],
+                                                      salearn_path => $salearn,
+                                                      count        => $count,
+                                                     };
         }
-        my $cmd = "$^X -T $salearn --ham -L --config-file=$salearncfg --prefs-file=$salearnprefs --siteconfigpath=$dstdir --dbpath=$dstdir/db --no-sync  '$dstdir/$easy_ham/*' 2>&1";
-        print STDERR "# $cmd\n" if $options->{verbose} >= 4;
-        print STDERR "# Run...\n" if $options->{verbose} >= 3;
-
-        my @output;
-        my $t = timeit $count, sub { @output = map { chomp; $_ } qx($cmd) };
-
-        my $maxerr = ($#output < 10) ? $#output : 10;
-        print STDERR join("\n# ", "", @output[0..$maxerr])    if $options->{verbose} >= 4;
-
-        return {
-                salearn => {
-                            Benchmark    => $t,
-                            salearn_path => $salearn,
-                            count        => $count,
-                           }
-               };
+        return \%results;
 }
 
 1;
