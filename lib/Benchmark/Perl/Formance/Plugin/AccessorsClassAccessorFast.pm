312717e5501f85025f45a28c5c7c07e9487d6b34
Steffen Schwigon <ss5@renormalist.net>
2015-09-03 15:46:53 +0200

    several 'Accessors*' plugins
    - selfmade HASH
    - selfmade ARRAY
    - Class::MethodMaker
    - Class::Accessors
    - Class::Accessors::Fast
    - Class::XSAccessors
    - Class::XSAccessors::Array
    - Object::Tiny::RW
    - Moose
    - Mouse

---
+++ lib/Benchmark/Perl/Formance/Plugin/AccessorsClassAccessorFast.pm
@@ -0,0 +1,49 @@
+package Benchmark::Perl::Formance::Plugin::AccessorsClassAccessorFast;
+# ABSTRACT: benchmark plugin - Compare OO'ish accessors
+
+use strict;
+use warnings;
+
+our $VERSION = "0.001";
+
+#############################################################
+#                                                           #
+# Benchmark Code ahead - Don't touch without strong reason! #
+#                                                           #
+#############################################################
+
+our $goal;
+our $count;
+
+use Benchmark ':hireswallclock';
+
+use base "Class::Accessor::Fast";
+__PACKAGE__->mk_accessors( "zomtec" );
+
+sub main
+{
+        my ($options) = @_;
+
+        # ensure same values over all Accessors* plugins!
+        $goal  = $options->{fastmode} ? 100_000 : 2_000_000;
+        $count = 5;
+
+        my $result;
+        my $obj = __PACKAGE__->new;
+        my $t_get   = timeit $count, sub { $result = $obj->zomtec()   for 1..$goal };
+        my $t_set   = timeit $count, sub { $result = $obj->zomtec(23) for 1..$goal };
+        return {
+                get => {
+                        Benchmark => $t_get,
+                        result    => $result,
+                        goal      => $goal,
+                       },
+                set => {
+                        Benchmark => $t_set,
+                        result    => $result,
+                        goal      => $goal,
+                       },
+               };
+}
+
+1;
