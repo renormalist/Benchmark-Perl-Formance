06ea8974b7f12bdca2b9cf29c77193311de40892
Steffen Schwigon <ss5@renormalist.net>
2015-09-03 18:45:19 +0200

    yet another accessor benchmark: Moo

---
+++ lib/Benchmark/Perl/Formance/Plugin/AccessorsMoo.pm
@@ -0,0 +1,50 @@
+package Benchmark::Perl::Formance::Plugin::AccessorsMoo;
+# ABSTRACT: benchmark plugin - Compare OO'ish accessors
+
+use Moo;
+
+use strict;
+use warnings;
+
+our $VERSION = "0.001";
+
+#############################################################
+#                                                           #
+# Benchmark Code ahead - Don't touch without strong reason! #
+#                                                           #
+#############################################################
+
+our $goal;
+our $count;
+
+use Benchmark ':hireswallclock';
+
+has 'zomtec' => (is => 'rw', default => 42);
+
+sub main
+{
+        my ($options) = @_;
+
+        # ensure same values over all Accessors* plugins!
+        $goal  = $options->{fastmode} ? 100_000 : 2_000_000;
+        $count = 5;
+
+        my $result;
+        my $obj = __PACKAGE__->new;
+        my $t_get   = timeit $count, sub { $result = $obj->zomtec()   for 1..$goal };
+        my $t_set   = timeit $count, sub { $result = $obj->zomtec(23) for 1..$goal };
+        return {
+                get => {
+                        Benchmark => $t_get,
+                        result    => $result,
+                        goal      => $goal,
+                       },
+                set => {
+                        Benchmark => $t_set,
+                        result    => $result,
+                        goal      => $goal,
+                       },
+               };
+}
+
+1;
